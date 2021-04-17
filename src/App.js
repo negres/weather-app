07b9868f804866d171c6fab3d2dfaba2e5389d16
Natalia Negreiros <nataliaknegres@gmail.com>
2021-04-17 13:20:34 -0300

    add all files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [location, setLocation] = useState(false);
+  const [weather, setWeather] = useState(false);
+
+  const appIdKey = `fa8f3a2f4d059a0d9fa0af2d23cd7d13`;
+
+  let getWeather = async (lat, long, appid) => {
+    let response = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${appid}&units=metric`
+    );
+    setWeather(response.data);
+  };
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      getWeather(position.coords.latitude, position.coords.longitude, appIdKey);
+      setLocation(true);
+    });
+  }, [appIdKey]);
+
+  if (location === false) {
+    return (
+      <div>
+        <h1>Você precisa habilitar a localização no browser!</h1>
+      </div>
+    );
+  } else if (weather === false) {
+    return (
+      <h1>Carregando...</h1>
+    );
+  } else {
+    const { main } = weather;
+    return (
+      <div className="App">
+        <h3>Clima nas suas coordenadas </h3>
+        <ul>
+          <li>Temperatura atual: {Math.trunc(main.temp)}°C</li>
+          <li>Temperatura máxima: {Math.trunc(main.temp_max)}°C</li>
+          <li>Temperatura mínima: {Math.trunc(main.temp_min)}°C</li>
+          <li>Umidade: {weather.main.humidity}</li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
